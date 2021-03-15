7b8c9d84d5a8095062f381b71004c1e4a5111b3e
Taylor H <taylor@htmlartist.net>
2021-03-15 19:10:23 -0400

    done except bonus

--- test/server.js
+++ test/server.js
@@ -1,55 +1,54 @@
 const http = require('http');
 
 const server = http.createServer(function(req, res) {
-    if (req.url === "/OK") {
+  if (req.url === "/OK") {
     console.log('Inbound OK, request being processed');
     res.writeHead(200);
     res.end();
-    } else if (req.url === "/Bad-Request"){
-        console.log("Bad request");
-        res.writeHead(400);
-        res.end();
-    } else if (req.url === "/Created") {
-        console.log("Created");
-        res.writeHead(201);
-        res.end();
-    } else if (req.url === "/Forbidden") {
-        console.log("Forbidden");
-        res.writeHead(403);
-        res.end();
-    } else if (req.url === "/Found") {
-        console.log("Found");
-        res.writeHead(302);
-        res.end();
-    } else if (req.url === "/Gateway-Timeout") {
-        console.log("Gateway Timeout");
-        res.writeHead(504);
-        res.end();
-    } else if (req.url === "/Internal-Server-Error") {
-        console.log("Internal Server Error");
-        res.writeHead(500);
-        res.end();
-    } else if (req.url === "/Moved-Permanently") {
-        console.log("Moved Permanently");
-        res.writeHead(301);
-        res.end();
-    } else if (req.url === "/Unauthorized") {
-        console.log("Unauthorized");
-        res.writeHead(401);
-        res.end();
-    } else if (req.url === "/Bonus/Redirect"){
-        console.log("Redirecting");
-        res.writeHead(308);
-        res.end();
-    } else if (req.url === "/Bonus/Created") {
-        console.log("Created");
-        res.writeHead(201);
-        res.end();
-    }
-    else {
-        res.writeHead(404);
-        res.end();
-    }
+  } else if (req.url === "/Bad-Request") {
+    console.log("Bad request");
+    res.writeHead(400);
+    res.end();
+  } else if (req.url === "/Created") {
+    console.log("Created");
+    res.writeHead(201);
+    res.end();
+  } else if (req.url === "/Forbidden") {
+    console.log("Forbidden");
+    res.writeHead(403);
+    res.end();
+  } else if (req.url === "/Found") {
+    console.log("Found");
+    res.writeHead(302);
+    res.end();
+  } else if (req.url === "/Gateway-Timeout") {
+    console.log("Gateway Timeout");
+    res.writeHead(504);
+    res.end();
+  } else if (req.url === "/Internal-Server-Error") {
+    console.log("Internal Server Error");
+    res.writeHead(500);
+    res.end();
+  } else if (req.url === "/Moved-Permanently") {
+    console.log("Moved Permanently");
+    res.writeHead(301);
+    res.end();
+  } else if (req.url === "/Unauthorized") {
+    console.log("Unauthorized");
+    res.writeHead(401);
+    res.end();
+    // } else if (req.url === "/Bonus/Redirect"){
+    //     console.log("Redirecting");
+    //     res.writeHead(308);
+    //     res.end();
+    // } else if (req.url === "/Bonus/Created") {
+    //     console.log("Created");
+    //     res.writeHead(201);
+    //     res.end();
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(3000, function() {
